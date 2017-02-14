1ffa430273f8721466f4824a2aa779cabbaf95b9
Dmitry Fisenko <fisenkodv@gmail.com>
2017-02-13 21:11:32 -0500

    Remove dist folder

--- Itinerary.Web/wwwroot/dist/Client/shared/cache/storage/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./storage.browser"));
-__export(require("./storage.node"));
-__export(require("./storage.service"));
-//# sourceMappingURL=index.js.map
