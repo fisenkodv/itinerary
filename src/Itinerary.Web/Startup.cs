e86a25b1bed8b551c1e3a6aced30958de6a66de0
Dmitry Fisenko <fisenkodv@gmail.com>
2017-02-21 18:04:34 -0500

    Create test project

---
+++ src/Itinerary.Web/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Itinerary.Web
+{
+  public class Startup
+  {
+    public IConfigurationRoot Configuration { get; }
+
+    public Startup( IHostingEnvironment env, ILoggerFactory loggerFactory )
+    {
+      Configuration = new ConfigurationBuilder()
+        .SetBasePath( env.ContentRootPath )
+        .AddJsonFile( "appsettings.json", optional: true, reloadOnChange: true )
+        .AddJsonFile( $"appsettings.{env.EnvironmentName}.json", optional: true )
+        .AddEnvironmentVariables()
+        .Build();
+
+      loggerFactory.AddConsole( Configuration.GetSection( "Logging" ) );
+      loggerFactory.AddDebug();
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices( IServiceCollection services )
+    {
+      // Add framework services.
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure( IApplicationBuilder app, IHostingEnvironment env )
+    {
+      if ( env.IsDevelopment() )
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseWebpackDevMiddleware(
+          new WebpackDevMiddlewareOptions
+          {
+            HotModuleReplacement = true
+          } );
+      }
+      else
+      {
+        app.UseExceptionHandler( "/Home/Error" );
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(
+        routes =>
+        {
+          routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}" );
+
+          routes.MapSpaFallbackRoute(
+            name: "spa-fallback",
+            defaults: new { controller = "Home", action = "Index" } );
+        } );
+    }
+  }
+}
---
+++ test/Itinerary.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace Itinerary.Tests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+    }
+  }
+}
