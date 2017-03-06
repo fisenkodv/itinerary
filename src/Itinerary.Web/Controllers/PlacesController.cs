abb909d9aee42cf18b15fdda88f34c5d97cdee36
Dmitry Fisenko <fisenkodv@gmail.com>
2017-03-05 23:19:03 -0500

    Add places controller example

---
+++ src/Itinerary.Web/Controllers/PlacesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Itinerary.Common;
+using Itinerary.DataAccess.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Itinerary.Web.Controllers
+{
+  [Route( "api/[controller]" )]
+  public class PlacesController : Controller
+  {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public PlacesController( IUnitOfWork unitOfWork )
+    {
+      _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet( "[action]" )]
+    public IEnumerable<Place> Places()
+    {
+      return _unitOfWork.PlacesRepository.Get( _ => true );
+    }
+  }
+}
