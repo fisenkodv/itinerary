b6fffe55d9dac1169b2d1e246c7842d1124d5160
Dmitry Fisenko <fisenkodv@gmail.com>
2017-03-14 22:19:13 -0400

    Minor changes

--- src/Itinerary.Web/Client/main.browser.ts
+++ src/Itinerary.Web/Client/main.browser.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app';
 
 const rootElemTagName = 'itinerary-root';
 
