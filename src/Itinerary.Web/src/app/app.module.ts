b2665588895d5942ec3e187905a7f9982cdf5f45
Dmitry Fisenko <fisenkodv@gmail.com>
2017-03-14 21:18:15 -0400

    Initial version

--- src/Itinerary.Web/src/app/app.module.ts
+++ src/Itinerary.Web/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import 'hammerjs';
+import '../styles/styles.scss';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
