373df6efd86ff571645636bf40bd8924ea5ace96
Dmitry Fisenko <fisenkodv@gmail.com>
2017-05-07 20:57:21 -0400

    Cleanup

--- src/Itinerary.Web/Client/app/app.module.ts
+++ src/Itinerary.Web/Client/app/app.module.ts
@@ -1,11 +1,10 @@
-﻿import { ApplicationRef, NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Http, HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { createInputTransfer, createNewHosts, removeNgStyles } from '@angularclass/hmr';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -15,7 +14,6 @@ import '../styles/styles.scss';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppState, InternalStateType } from './app.service';
 import { AuthModule } from './auth';
 import { envProviders } from './environment';
 import { FooterComponent } from './footer/footer.component';
@@ -27,15 +25,8 @@ import { SigninDialogComponent } from './signin/signin-dialog.component';
 
 const appProviders = [
   PlacesService,
-  GooglePlacesService,
-  AppState
+  GooglePlacesService
 ];
-
-interface StoreType {
-  state: InternalStateType,
-  restoreInputValues: () => void,
-  disposeOldHosts: () => void
-}
 
 export function createTranslateLoader(http: Http) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -62,7 +53,7 @@ export function createTranslateLoader(http: Http) {
         deps: [Http]
       }
     }),
-    MaterialModule.forRoot(),
+    MaterialModule,
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
@@ -79,45 +70,6 @@ export function createTranslateLoader(http: Http) {
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(
-    public appRef: ApplicationRef,
-    public appState: AppState) {
-  }
-
-  public hmrOnInit(store: StoreType) {
-    if (!store || !store.state) {
-      return;
-    }
-    console.log('HMR store', JSON.stringify(store, null, 2));
-    // set state
-    this.appState._state = store.state;
-    // set input values
-    if ('restoreInputValues' in store) {
-      const restoreInputValues = store.restoreInputValues;
-      setTimeout(restoreInputValues);
-    }
-
-    this.appRef.tick();
-    delete store.state;
-    delete store.restoreInputValues;
-  }
-
-  public hmrOnDestroy(store: StoreType) {
-    const cmpLocation = this.appRef.components.map((cmp) => cmp.location.nativeElement);
-    // save state
-    const state = this.appState._state;
-    store.state = state;
-    // recreate root elements
-    store.disposeOldHosts = createNewHosts(cmpLocation);
-    // save input values
-    store.restoreInputValues = createInputTransfer();
-    // remove styles
-    removeNgStyles();
-  }
-
-  public hmrAfterDestroy(store: StoreType) {
-    // display new elements
-    store.disposeOldHosts();
-    delete store.disposeOldHosts;
+  constructor() {
   }
 }
