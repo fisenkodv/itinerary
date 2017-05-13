ad2c6eb847bac2ed5e7ad86b5aef00cc0ebb4ba9
Dmitry Fisenko <fisenkodv@gmail.com>
2017-05-12 23:02:12 -0400

    Fix path Add favicon

--- src/Itinerary.Web/src/client/app/core/base.service.ts
+++ src/Itinerary.Web/src/client/app/core/base.service.ts
@@ -3,7 +3,7 @@ import { Config } from '../shared/config/env.config';
 export abstract class BaseService {
   protected getBaseServiceUrl(appendVersion: boolean = true): string {
     const version = appendVersion ? `api/${Config.APIVersion}` : '';
-    return `${Config.API}${version}/`;
+    return `${Config.API}${version}`;
   }
 
   protected urlEncode(obj: any): string {
