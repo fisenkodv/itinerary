04e872048c32df012cba82d63dfe8f782c7e007e
Dmitry Fisenko <fisenkodv@gmail.com>
2017-05-29 10:27:18 -0700

    Move translation module initialization to separete file

--- src/Itinerary.Web/src/client/app/app.module.ts
+++ src/Itinerary.Web/src/client/app/app.module.ts
@@ -6,8 +6,6 @@ import { Http, HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { EffectsModule } from '@ngrx/effects';
 import { RouterStoreModule } from '@ngrx/router-store';
@@ -24,12 +22,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { PlacesModule } from './places/places.module';
+import { TranslationModule } from './translation.module';
 
 import { AppComponent } from './app.component';
-
-export function CreateTranslateLoader(http: Http) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 @NgModule({
   declarations: [AppComponent],
@@ -41,13 +36,7 @@ export function CreateTranslateLoader(http: Http) {
     ReactiveFormsModule,
     HttpModule,
     FlexLayoutModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (CreateTranslateLoader),
-        deps: [Http]
-      }
-    }),
+    TranslationModule.forRoot(),
     Effects,
     StoreModule.provideStore(AppReducer, InitialState),
     RouterStoreModule.connectRouter(),
---
+++ src/Itinerary.Web/src/client/app/translation.module.ts
@@ -0,0 +1,23 @@
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+import { Http, HttpModule } from '@angular/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export class TranslationModule {
+  public static forRoot(): ModuleWithProviders {
+    return TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: Http) => new TranslateHttpLoader(http, './assets/i18n/', '.json'),
+        deps: [Http]
+      }
+    })
+  }
+
+  constructor( @Optional() @SkipSelf() parentModule: TranslateModule) {
+    if (parentModule) {
+      throw new Error(
+        'TranslateModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
