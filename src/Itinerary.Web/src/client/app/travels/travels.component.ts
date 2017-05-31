ee64c84fd3ccaa583e569e7e98e68cfaf08f4067
Dmitry Fisenko <fisenkodv@gmail.com>
2017-05-31 16:44:15 -0400

    Add grid list example

--- src/Itinerary.Web/src/client/app/travels/travels.component.ts
+++ src/Itinerary.Web/src/client/app/travels/travels.component.ts
@@ -7,4 +7,19 @@
   styleUrls: ['travels.component.css']
 })
 export class TravelsComponent {
+  public places = [
+    { rows: 2, name: 'Saratov' },
+    { rows: 1, name: 'Prague' },
+    { rows: 1, name: 'Thailand' },
+    { rows: 1, name: 'Moscow' },
+    { rows: 1, name: 'StPetersburg' },
+    { rows: 2, name: 'Volgograd' },
+    { rows: 1, name: 'NY' },
+    { rows: 1, name: 'LasVegas' },
+    { rows: 1, name: 'Chicago' }
+  ];
+
+  public showPlace(place: any) {
+    return true;
+  }
 }
