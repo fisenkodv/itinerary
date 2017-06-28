deaba8ca15de7efe2c45a06efedaa8ac13e8838d
Dmitry Fisenko <fisenkodv@gmail.com>
2017-06-27 21:00:31 -0400

    Add animation

--- src/Itinerary.Web/src/client/app/places/place-list-item/place-list-item.component.ts
+++ src/Itinerary.Web/src/client/app/places/place-list-item/place-list-item.component.ts
@@ -1,13 +1,17 @@
 ﻿import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 import { PlaceDetails } from '../models/index';
+import { fadeInOut } from '../../shared/animations/fade-in-out';
 
 @Component({
   moduleId: module.id,
   selector: 'place-list-item',
   templateUrl: 'place-list-item.component.html',
   styleUrls: ['place-list-item.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    fadeInOut
+  ]
 })
 export class PlaceListItemComponent {
   @Input() public place: PlaceDetails;
--- src/Itinerary.Web/src/client/app/shared/animations/fade-in-out.ts
+++ src/Itinerary.Web/src/client/app/shared/animations/fade-in-out.ts
@@ -1,15 +1,11 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 export const fadeInOut = trigger('fadeInOut', [
   transition(':enter', [
     style({ opacity: 0 }),
-    animate('100ms ease-in')
+    animate('100ms ease-in', style({ opacity: 1 })),
   ]),
-  transition(':leave', animate('100ms ease-out', style({ opacity: 0 })))
+  transition(':leave', [
+    animate('100ms ease-out', style({ opacity: 0 }))
+  ])
 ]);
