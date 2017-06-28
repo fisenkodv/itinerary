b5088e81fdfcfe35518ed34753f0cd640e6897d8
Dmitry Fisenko <fisenkodv@gmail.com>
2017-06-27 23:29:58 -0400

    Fix prod build

--- src/Itinerary.Web/tools/config/seed.config.ts
+++ src/Itinerary.Web/tools/config/seed.config.ts
@@ -434,7 +434,10 @@ export class SeedConfig {
     base: this.PROJECT_ROOT,
     packageConfigPaths: [
       join('node_modules', '*', 'package.json'),
-      join('node_modules', '@angular', '*', 'package.json')
+      join('node_modules', '@angular', '*', 'package.json'),
+      join('node_modules', '@ngrx', '*', 'package.json'),
+      join('node_modules', '@ngx-translate', '*', 'package.json'),
+      join('node_modules', '@agm', '*', 'package.json'),
       // for other modules like @ngx-translate the package.json path needs to updated here
       // otherwise npm run build.prod would fail
       // join('node_modules', '@ngx-translate', '*', 'package.json')
