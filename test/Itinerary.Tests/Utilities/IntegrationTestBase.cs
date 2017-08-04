923f08816fc61df2d864b0543e788e00c8a60612
Dmitry Fisenko <fisenkodv@gmail.com>
2017-08-03 23:41:57 -0400

    Add base class for integration tests

---
+++ test/Itinerary.Tests/Utilities/IntegrationTestBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Itinerary.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Itinerary.Tests.Utilities
+{
+  public class IntegrationTestBase
+  {
+    private readonly TestServer _testServer;
+    private readonly HttpClient _httpClient;
+
+    public IntegrationTestBase()
+    {
+      _testServer = new TestServer( new WebHostBuilder().UseStartup<Startup>() );
+      _httpClient = _testServer.CreateClient();
+    }
+
+    public async Task<string> Get( string url )
+    {
+      HttpResponseMessage response = await _httpClient.GetAsync( url );
+      return await response.Content.ReadAsStringAsync();
+    }
+  }
+}
