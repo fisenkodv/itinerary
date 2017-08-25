950a0833aab4c510e45f77a38b40790b216d203d
Dmitry Fisenko <fisenkodv@gmail.com>
2017-08-25 00:22:08 -0400

    Add code to transform data

--- test/Itinerary.Tests/Unit/RankGeneratorTests.cs
+++ test/Itinerary.Tests/Unit/RankGeneratorTests.cs
@@ -1,14 +1,45 @@
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using FluentAssertions;
 using Itinerary.Common;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Itinerary.Tests.Unit
 {
   public class RankGeneratorTests
   {
+    [Fact]
+    public void Transform()
+    {
+      string fileName =
+        @"C:\Users\fisenkodv\Desktop\Projects\itinerary\src\Itinerary.DataAccess\Data\PlacesSnapshot.Production.json";
+
+      var places = JsonConvert.DeserializeObject<List<PlaceSnapshotItem>>( File.ReadAllText( fileName ) );
+
+      var reviews =
+        places.Select( x => new { reviews = x.Reviews, rating = x.Rating } ).ToLookup( x => x.rating );
+
+      var results = new ConcurrentBag<(int reviews, double rating, List<Rank> ranks)>();
+      reviews.AsParallel().ForAll(
+        x =>
+        {
+          x.AsParallel().ForAll(
+            rating =>
+            {
+              results.Add(
+                (rating.reviews, x.Key,
+                RankGenerator.GetRanks( rating.reviews, x.Key ).ToList() ) );
+            } );
+        } );
+
+      File.WriteAllText( "out.json", JsonConvert.SerializeObject( results ) );
+    }
+
     [Fact]
     public void It_should_return_ranks()
     {
@@ -31,5 +62,48 @@ namespace Itinerary.Tests.Unit
       ranks.Should().NotBeNullOrEmpty();
       rank.StandardDeviation.Should().Be( ranks.Min( x => x.StandardDeviation ) );
     }
+  }
+
+  internal class PlaceSnapshotItem
+  {
+    public string Name { get; set; }
+
+    public double Rating { get; set; }
+
+    public int Reviews { get; set; }
+
+    public IReadOnlyList<string> Categories { get; set; }
+
+    public string Url { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+  }
+
+  internal class PlaceSnapshotItemUpdated
+  {
+    public string Name { get; set; }
+
+    public double Rating { get; set; }
+
+    public List<Review> Reviews { get; set; }
+
+    public List<string> Categories { get; set; }
+
+    public string Url { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+  }
+
+  public class Review
+  {
+    public int Rating { get; set; }
   }
 }
