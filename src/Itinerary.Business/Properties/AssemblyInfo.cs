64488377a3ed976fd4d799ce32779dc3582dc28b
Dmitry Fisenko <fisenkodv@gmail.com>
2017-08-29 13:16:57 -0400

    Add shared version info

---
+++ src/Itinerary.Business/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle( "Itinerary.Business" )]
+[assembly: AssemblyDescription( "Itinerary.Business" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
