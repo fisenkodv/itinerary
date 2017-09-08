af497af425f617be5dd9760a3f1ac791e6034b3c
Dmitry Fisenko <fisenkodv@gmail.com>
2017-09-07 21:09:03 -0400

    Update version

--- src/Version.cs
+++ src/Version.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Dmitry Fisenko")]
@@ -10,11 +10,11 @@ using System.Reflection;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
