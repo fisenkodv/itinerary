3cc8513ef22d3822e5a61817de651ed2ebcb5024
Dmitry Fisenko <fisenkodv@gmail.com>
2018-01-31 17:47:00 -0500

    Update app module

--- src/Itinerary.Web/src/app/app.module.ts
+++ src/Itinerary.Web/src/app/app.module.ts
@@ -3,8 +3,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
