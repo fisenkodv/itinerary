4de4de56d6cdd349de8a53ff8e046e347d4622ce
Dmitry Fisenko <fisenkodv@gmail.com>
2018-04-14 13:22:21 -0400

    Revert experiments back :)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  google: {
+    maps: { apiKey: 'AIzaSyBkxFjVilFXxTMLb6JxL2x-AGeCOLIRO1M', libraries: ['places'] },
+    places: { apiKey: 'AIzaSyAHtJrpKyxsARFESBwI-a0qaWjsD5ZW-3w' }
+  },
+  firebase: {
+    apiKey: 'AIzaSyCuUCX9rXofzOB0u0TFXQcYu20u4_54S3c',
+    authDomain: 'itinerary-1488041928048.firebaseapp.com',
+    databaseURL: 'https://itinerary-1488041928048.firebaseio.com',
+    projectId: 'itinerary-1488041928048',
+    storageBucket: 'itinerary-1488041928048.appspot.com',
+    messagingSenderId: '733435504069'
+  }
 };
