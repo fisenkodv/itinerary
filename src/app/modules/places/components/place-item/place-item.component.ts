01ce59da13722d2b6a07f7b29c521514cf3d987f
Dmitry Fisenko <fisenkodv@gmail.com>
2018-05-09 16:23:00 -0400

    Fix tiles

--- src/app/modules/places/components/place-item/place-item.component.ts
+++ src/app/modules/places/components/place-item/place-item.component.ts
@@ -8,4 +8,20 @@ import { Place } from '@app/modules/places/models';
 })
 export class PlaceItemComponent {
   @Input() place: Place;
+
+  public getCategoriesText() {
+    const numberOfCategories = this.place.categories.length;
+    let categories = '';
+    if (numberOfCategories) {
+      categories = this.place.categories[0];
+      if (numberOfCategories > 1) {
+        categories += '...';
+      }
+    }
+    return categories;
+  }
+
+  public getCategoriesTooltipText() {
+    return this.place.categories.join(', ');
+  }
 }
